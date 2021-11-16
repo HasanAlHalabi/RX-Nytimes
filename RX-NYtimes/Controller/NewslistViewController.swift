9566eba58fff8bad5d03b91af9d5bfcb23e02d05
hasan al halabi <hasan.said.halabi@gmail.com>
2021-11-16 12:01:43 +0200

    MVVM WITH Rxswift

--- RX-NYtimes/Controller/NewslistViewController.swift
+++ RX-NYtimes/Controller/NewslistViewController.swift
@@ -10,47 +10,88 @@ import RxCocoa
 import RxSwift
 
 
-class NewslistViewController: UIViewController, UITableViewDelegate {
-   var newslistViewModel = NewslistViewModel()
+class NewslistViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+ 
+    
+   
+    var newslistViewModel : NewslistViewModel!
     var bag = DisposeBag()
+    var newfetchdata = NewsRepoApi()
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
-       
+        tableView.dataSource = self
+       fetchdata()
         tableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "MessageCell")
         tableView.separatorColor = UIColor.darkGray
         tableView.separatorInset = .zero
         navigationItem.title="Ny Time Most Popular Article"
-        bindListData()
+//        bindListData()
         // Do any additional setup after loading the view.
     }
-
-    func bindListData(){
-        newslistViewModel.articles.bind(to: tableView.rx.items(cellIdentifier: "MessageCell", cellType: MessageCell.self)){ row,iten, cell in
-           
-            cell.titleLable.text = iten.title
-            cell.ByName.text = iten.byline
-            cell.DateText.text = iten.published_date
-            let mediaMetaData = iten.media?.first?.metadata
-            
-            cell.imagev.load(urlString: mediaMetaData?.first?.url ?? "https://1000logos.net/wp-content/uploads/2017/04/Symbol-New-York-Times.png")
-        }.disposed(by: bag)
-        tableView.rx.modelSelected(Post.self).bind{
-            article in
-            let vc = self.storyboard?.instantiateViewController(withIdentifier: "DetailsNews")as! DetailsViewController
-            vc.titletext1 = article.title
-            vc.updated = article.updated
-            vc.byLine = article.byline
-            vc.abstrack1 = article.abstract
-            vc.keyword = article.adx_keywords
-            vc.imagelink = article.media?.first?.metadata[2].url ?? "https://1000logos.net/wp-content/uploads/2017/04/Symbol-New-York-Times.png"
-            self.navigationController?.pushViewController(vc, animated: true)
-        }.disposed(by: bag)
-        newslistViewModel.fetchdata()
-    }
-
     
+    func fetchdata(){
+        
+        newfetchdata.fetchallData{ result in
+            self.newslistViewModel = NewslistViewModel(result)
+            DispatchQueue.main.async {
+               self.tableView.reloadData()
+            }
+        }
+    }
+//    func bindListData(){
+//        newslistViewModel.articles.bind(to: tableView.rx.items(cellIdentifier: "MessageCell", cellType: MessageCell.self)){ row,iten, cell in
+//
+//            cell.titleLable.text = iten.title
+//            cell.ByName.text = iten.byline
+//            cell.DateText.text = iten.published_date
+//            let mediaMetaData = iten.media?.first?.metadata
+//
+//            cell.imagev.load(urlString: mediaMetaData?.first?.url ?? "https://1000logos.net/wp-content/uploads/2017/04/Symbol-New-York-Times.png")
+//        }.disposed(by: bag)
+//        tableView.rx.modelSelected(Post.self).bind{
+//            article in
+//            let vc = self.storyboard?.instantiateViewController(withIdentifier: "DetailsNews")as! DetailsViewController
+//            vc.titletext1 = article.title
+//            vc.updated = article.updated
+//            vc.byLine = article.byline
+//            vc.abstrack1 = article.abstract
+//            vc.keyword = article.adx_keywords
+//            vc.imagelink = article.media?.first?.metadata[2].url ?? "https://1000logos.net/wp-content/uploads/2017/04/Symbol-New-York-Times.png"
+//            self.navigationController?.pushViewController(vc, animated: true)
+//        }.disposed(by: bag)
+//
+//    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MessageCell") as! MessageCell
+        cell .imagev.load(urlString: newslistViewModel.articlesVM[indexPath.row].imageurl)
+        cell.titleLable.text = newslistViewModel.articlesVM[indexPath.row].title
+        cell.ByName.text = newslistViewModel.articlesVM[indexPath.row].byLine
+        cell.DateText.text = newslistViewModel.articlesVM[indexPath.row].publishdate
+        return cell
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+           return self.newslistViewModel == nil ? 0 : self.newslistViewModel.numberOfSections
+       }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return  self.newslistViewModel.numberOfRowsInSection(section)
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return 133
+        }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+      let  selectednews = self.newslistViewModel.newsAtindex(indexPath.row)
+        let vc = storyboard?.instantiateViewController(withIdentifier: "DetailsNews")as! DetailsViewController
+               vc.titletext1 = selectednews.title
+        vc.abstrack1 = selectednews.abstract
+        vc.byLine = selectednews.byLine
+        vc.imagelink = selectednews.thumb
+        vc.updated = selectednews.updated
+               navigationController?.pushViewController(vc, animated: true)
+    }
     
 }
 
--- RX-NYtimes/ViewModel/NewsListViewModel.swift
+++ RX-NYtimes/ViewModel/NewsListViewModel.swift
@@ -9,12 +9,52 @@ import Foundation
 import RxCocoa
 import RxSwift
 struct NewslistViewModel{
-    let articles = PublishSubject<[Post]>()
-    var newfetchdata = NewsRepoApi()
-    func fetchdata(){
-        newfetchdata.fetchallData{ result in
-            articles.onNext(result)
-            articles.onCompleted()
+    let articlesVM : [NewsModel]
+    func numberOfRowsInSection(_ section: Int)-> Int{
+           return self.articlesVM.count
+       }
+    func newsAtindex(_ index: Int ) -> NewsModel{
+            let news = self.articlesVM[index]
+        return NewsModel(news.article)
         }
+    var numberOfSections : Int {
+           return 1
+       }
+}
+extension NewslistViewModel{
+    init(_ articles: [Post]){
+        self.articlesVM = articles.compactMap(NewsModel.init)
+    }
+}
+struct NewsModel{
+    let article : Post
+    init(_ article : Post){
+        self.article = article
+    }
+}
+extension NewsModel{
+    var title : String{
+        return article.title
+    }
+    var byLine : String{
+        return article.byline
+    }
+    var updated : String{
+        return article.updated
+    }
+    var imageurl : String{
+        return article.media?.first?.metadata[0].url ?? "https://1000logos.net/wp-content/uploads/2017/04/Symbol-New-York-Times.png"
+    }
+    var thumb : String{
+        return article.media?.first?.metadata[2].url ?? "https://1000logos.net/wp-content/uploads/2017/04/Symbol-New-York-Times.png"
+    }
+    var publishdate : String{
+        return article.published_date
+    }
+    var keys : String{
+        return article.adx_keywords
+    }
+    var abstract : String{
+        return article.abstract
     }
 }
